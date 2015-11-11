ac9e667f1ccb24ac07eb295b2d993801b9c534a2
dudesoft <andreas@pritschet.eu>
2015-11-11 01:04:55 +0100

    able to display first markers

---
+++ hoerburger/config/initializers/leaflet.rb
@@ -0,0 +1,3 @@
+Leaflet.tile_layer = "https://api.tiles.mapbox.com/v4/andipri.o4il298m/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYW5kaXByaSIsImEiOiJjaWd0c2l0cnMwMGY1dnZrbGV1ZzdhNmwzIn0.MJRx4pntHRsGq17ZJ6xBSQ"
+Leaflet.attribution = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+Leaflet.max_zoom = 10;
