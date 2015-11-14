900cefda7dda9f7ba72c431d98580a3c65ffa742
dudesoft <andreas@pritschet.eu>
2015-11-14 18:43:14 +0100

    Basic Map functionality

--- hoerburger/app/controllers/content_controller.rb
+++ hoerburger/app/controllers/content_controller.rb
@@ -1,11 +1,4 @@
 class ContentController < ApplicationController
   def home
-    @locations = Array.new
-
-    Travel.all.each do |t|
-      t.locations.all.each do |l|
-        @locations.append(:latlng => [l.longitude, l.latitude])
-      end
-    end
   end
 end
--- hoerburger/config/initializers/leaflet.rb
+++ hoerburger/config/initializers/leaflet.rb
@@ -1,3 +1,5 @@
-Leaflet.tile_layer = "https://api.tiles.mapbox.com/v4/andipri.o4il298m/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYW5kaXByaSIsImEiOiJjaWd0c2l0cnMwMGY1dnZrbGV1ZzdhNmwzIn0.MJRx4pntHRsGq17ZJ6xBSQ"
+MAPBOX_API = "https://api.tiles.mapbox.com/v4/andipri.o4il298m/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYW5kaXByaSIsImEiOiJjaWd0c2l0cnMwMGY1dnZrbGV1ZzdhNmwzIn0.MJRx4pntHRsGq17ZJ6xBSQ"
+
+Leaflet.tile_layer = MAPBOX_API
 Leaflet.attribution = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-Leaflet.max_zoom = 10;
+Leaflet.max_zoom = 15;
