80dac99925cf8dcca5027b556c25a41fe2c03056
ahansbauer <Andrea.Hansbauer@stud.uni-regensburg.de>
2016-01-23 10:39:29 +0100

    impressum link

--- WebProject/footer.php
+++ WebProject/footer.php
@@ -1,6 +1,6 @@
 <footer class="navigation_element">
     <div id="link_container">
-        <a class="footer_link" href="url">Über Hoerburger</a>
+        <a class="footer_link" href="impressum.php">Über Hoerburger</a>
         <a class="footer_link" href="url">Über Uns</a>
         <a class="footer_link" href="url">Impressum</a>
     </div>
