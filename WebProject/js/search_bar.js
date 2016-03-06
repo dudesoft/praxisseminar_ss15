ea30b6cafafddff896b71c447e663f02fa53ffa8
MaxSchu <Maximilian.Schubert@stud.uni-regensburg.de>
2016-03-06 19:03:50 +0100

    search with enter

--- WebProject/js/search_bar.js
+++ WebProject/js/search_bar.js
@@ -3,7 +3,8 @@ define(['jquery', 'bootstrap'], function($) {
         window.open("search_result.php?search=" + $('#search-input').val(), "_self");
     });
 
-    $('search-input').keypress(function(event) {
+    $('#search-input').keypress(function(event) {
+        console.log('keypress lol');
         if (event.which == 13) {
             event.preventDefault();
             window.open("search_result.php?search=" + $('#search-input').val(), "_self");
