5a6ce204c7a8b6934826dbf21f3ca506aa84d08c
MaxSchu <Maximilian.Schubert@stud.uni-regensburg.de>
2016-03-31 13:33:01 +0200

    cleanup code and project

--- WebProject/js/app/search_bar.js
+++ WebProject/js/app/search_bar.js
@@ -1,162 +1,165 @@
 define(['jquery', './db_connector', 'bootstrap', 'jquery_ui'], function($, db_connector) {
-    db_connector.getAllJourneys(setupJourneyDropdown);
-    db_connector.getAllDates(setupDatepicker);
-    db_connector.getAutocompleteData(setupAutocomplete);
+    var SearchBar = {
+        setupSearchBar: function(src) {
+            //Database requests
+            db_connector.getAllJourneys(this.setupJourneyDropdown);
+            db_connector.getAllDates(this.setupDatepicker);
+            db_connector.getAutocompleteData(this.setupAutocomplete);
 
-    $('#search-button, #enter_search').click(function() {
-        buildSearchURL();
-    });
-
-    $('#search-input').keypress(function(event) {
-        if (event.which == 13) {
-            event.preventDefault();
-            buildSearchURL();
-        }
-    });
-
-    $('#advanced-search-button').on('click', function(event) {
-        $(this).parent().toggleClass('open');
-    });
-
-    $(document).on('click', '.ui-datepicker-next, .ui-datepicker-prev, .ui-datepicker', function(e) {
-        e.stopPropagation();
-    });
-
-    function setupAutocomplete(data) {
-        $("#search-input").autocomplete({
-            source: function(request, response) {
-                var term = $.ui.autocomplete.escapeRegex(request.term),
-                    startsWithMatcher = new RegExp("^" + term, "i"),
-                    startsWith = $.grep(data, function(value) {
-                        return startsWithMatcher.test(value.label || value.value || value);
-                    }),
-                    containsMatcher = new RegExp(term, "i"),
-                    contains = $.grep(data, function(value) {
-                        return $.inArray(value, startsWith) < 0 &&
-                            containsMatcher.test(value.label || value.value || value);
-                    });
-
-                response(startsWith.concat(contains));
-            }
-        });
-    }
-
-    function setupDatepicker(data) {
-        var datesObject = {};
-        data.forEach(function(date) {
-            cDate = new Date(date);
-            cDate.setHours(0);
-            cDate.setMinutes(0);
-            datesObject[cDate] = cDate;
-        });
-
-        $("#min_date_input").datepicker({
-            prevText: 'Zurück',
-            nextText: 'Vor',
-            todayText: 'heute',
-            closeText: 'schließen',
-            monthNames: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni',
-                'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'
-            ],
-            monthNamesShort: ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun',
-                'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'
-            ],
-            dayNames: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
-            dayNamesShort: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
-            dayNamesMin: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
-            showMonthAfterYear: false,
-            dateFormat: 'dd.mm.yy',
-            changeMonth: true,
-            changeYear: true,
-            minDate: new Date(data[0]),
-            maxDate: new Date(data[data.length - 1]),
-            beforeShowDay: function(date) {
-                var highlight = datesObject[date];
-                if (highlight) {
-                    return [true, 'existing-date', ''];
-                } else {
-                    return [true, '', ''];
+            //clicklisteners to start search
+            $('#search-button, #enter_search').click(function() {
+                SearchBar.buildSearchURL();
+            });
+            $('#search-input').keypress(function(event) {
+                if (event.which == 13) {
+                    event.preventDefault();
+                    SearchBar.buildSearchURL();
                 }
-            }
-        });
+            });
+            $('#advanced-search-button').on('click', function(event) {
+                $(this).parent().toggleClass('open');
+            });
 
-        $("#max_date_input").datepicker({
-            prevText: 'Zurück',
-            nextText: 'Vor',
-            todayText: 'heute',
-            closeText: 'schließen',
-            monthNames: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni',
-                'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'
-            ],
-            monthNamesShort: ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun',
-                'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'
-            ],
-            dayNames: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
-            dayNamesShort: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
-            dayNamesMin: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
-            showMonthAfterYear: false,
-            dateFormat: 'dd.mm.yy',
-            changeMonth: true,
-            changeYear: true,
-            minDate: new Date(data[0]),
-            maxDate: new Date(data[data.length - 1]),
-            beforeShowDay: function(date) {
-                var highlight = datesObject[date];
-                if (highlight) {
-                    return [true, 'existing-date', ''];
-                } else {
-                    return [true, '', ''];
+            $(document).on('click', '.ui-datepicker-next, .ui-datepicker-prev, .ui-datepicker', function(e) {
+                e.stopPropagation();
+            });
+        },
+        setupAutocomplete: function(data) {
+            $("#search-input").autocomplete({
+                source: function(request, response) {
+                    var term = $.ui.autocomplete.escapeRegex(request.term),
+                        startsWithMatcher = new RegExp("^" + term, "i"),
+                        startsWith = $.grep(data, function(value) {
+                            return startsWithMatcher.test(value.label || value.value || value);
+                        }),
+                        containsMatcher = new RegExp(term, "i"),
+                        contains = $.grep(data, function(value) {
+                            return $.inArray(value, startsWith) < 0 &&
+                                containsMatcher.test(value.label || value.value || value);
+                        });
+
+                    response(startsWith.concat(contains));
                 }
+            });
+        },
+        setupDatepicker: function(data) {
+            var datesObject = {};
+            data.forEach(function(date) {
+                cDate = new Date(date);
+                cDate.setHours(0);
+                cDate.setMinutes(0);
+                datesObject[cDate] = cDate;
+            });
+
+            $("#min_date_input").datepicker({
+                prevText: 'Zurück',
+                nextText: 'Vor',
+                todayText: 'heute',
+                closeText: 'schließen',
+                monthNames: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni',
+                    'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'
+                ],
+                monthNamesShort: ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun',
+                    'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'
+                ],
+                dayNames: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
+                dayNamesShort: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+                dayNamesMin: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+                showMonthAfterYear: false,
+                dateFormat: 'dd.mm.yy',
+                changeMonth: true,
+                changeYear: true,
+                minDate: new Date(data[0]),
+                maxDate: new Date(data[data.length - 1]),
+                beforeShowDay: function(date) {
+                    var highlight = datesObject[date];
+                    if (highlight) {
+                        return [true, 'existing-date', ''];
+                    } else {
+                        return [true, '', ''];
+                    }
+                }
+            });
+
+            $("#max_date_input").datepicker({
+                prevText: 'Zurück',
+                nextText: 'Vor',
+                todayText: 'heute',
+                closeText: 'schließen',
+                monthNames: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni',
+                    'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'
+                ],
+                monthNamesShort: ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun',
+                    'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'
+                ],
+                dayNames: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
+                dayNamesShort: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+                dayNamesMin: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+                showMonthAfterYear: false,
+                dateFormat: 'dd.mm.yy',
+                changeMonth: true,
+                changeYear: true,
+                minDate: new Date(data[0]),
+                maxDate: new Date(data[data.length - 1]),
+                beforeShowDay: function(date) {
+                    var highlight = datesObject[date];
+                    if (highlight) {
+                        return [true, 'existing-date', ''];
+                    } else {
+                        return [true, '', ''];
+                    }
+                }
+            });
+
+            $(document).on('click', '#main_content', function(e) {
+                $("#min_date_input").datepicker("hide");
+                $("#max_date_input").datepicker("hide");
+            });
+        },
+        setupJourneyDropdown: function(data) {
+            for (var i = 0; i < data.length; i++) {
+                $('#journey_dropdown').append($('<option>' + data[i].name + '</option>'))
             }
-        });
+        },
+        buildSearchURL: function() {
+            var minDate = "";
+            var maxDate = "";
+            var journey = "";
+            var resultType = "";
 
-        $(document).on('click', '#main_content', function(e) {
-            $("#min_date_input").datepicker("hide");
-            $("#max_date_input").datepicker("hide");
-        });
-    }
+            //Build strings for search url out of Search parameters
+            if ($('#min_date_input').val() != "") {
+                minDate = "&minDate=" + convertDateFormat($('#min_date_input').val());
+            }
+            if ($('#max_date_input').val() != "") {
+                maxDate = "&maxDate=" + convertDateFormat($('#max_date_input').val());
+            }
+            if ($('#journey_dropdown').find('option:selected').val() != "default") {
+                journey = "&journey=" + $('#journey_dropdown').val();
+            }
+            if ($('#result_type_dropdown').find('option:selected').val() != "default") {
+                resultType = "&resultType=" + $('#result_type_dropdown').find('option:selected').val();
+            }
 
-    function setupJourneyDropdown(data) {
-        for (var i = 0; i < data.length; i++) {
-            $('#journey_dropdown').append($('<option>' + data[i].name + '</option>'))
-        }
-    }
+            window.open("search_result.php?search=" + encodeURI($('#search-input').val()) + minDate + maxDate + journey + resultType, "_self");
+        },
+        convertDateFormat: function(date) {
+            var parts = date.split(".");
+            if (parts.length < 3) {
+                return null;
+            }
+            if (parts[1].length == 1) {
+                parts[1] = "0" + parts[1];
+            }
+            if (parts[0].length == 1) {
+                parts[0] = "0" + parts[0];
+            }
+            var newDate = parts[2] + "-" + parts[1] + "-" + parts[0];
 
-    function buildSearchURL() {
-        var minDate = "";
-        var maxDate = "";
-        var journey = "";
-        var resultType = "";
-
-        if ($('#min_date_input').val() != "") {
-            minDate = "&minDate=" + convertDateFormat($('#min_date_input').val());
-        }
-        if ($('#max_date_input').val() != "") {
-            maxDate = "&maxDate=" + convertDateFormat($('#max_date_input').val());
-        }
-        if ($('#journey_dropdown').find('option:selected').val() != "default") {
-            journey = "&journey=" + $('#journey_dropdown').val();
-        }
-        if ($('#result_type_dropdown').find('option:selected').val() != "default") {
-            resultType = "&resultType=" + $('#result_type_dropdown').find('option:selected').val();
+            return newDate;
         }
 
-        window.open("search_result.php?search=" + encodeURI($('#search-input').val()) + minDate + maxDate + journey + resultType, "_self");
-    }
-
-    function convertDateFormat(date) {
-        var parts = date.split(".");
-        if (parts.length < 3) {
-            return null;
-        }
-        if (parts[1].length == 1) {
-            parts[1] = "0" + parts[1];
-        }
-        if (parts[0].length == 1) {
-            parts[0] = "0" + parts[0];
-        }
-        var newDate = parts[2] + "-" + parts[1] + "-" + parts[0];
-
-        return newDate;
-    }
+    };
+    SearchBar.setupSearchBar();
+    return SearchBar;
 });
--- WebProject/js/app/search_main.js
+++ WebProject/js/app/search_main.js
@@ -1,97 +1,107 @@
 define(['jquery', './db_connector', './utils', './search_bar'], function($, dbConnector, utils) {
-    String.prototype.trunc =
-        function(n) {
-            return this.substr(0, n - 1) + (this.length > n ? '&hellip;' : '');
-        };
-
     var urlResult_constant = "&resultType=";
     var urlId_constant = "&objectId=";
-
     var urlVars = utils.getUrlVars();
-    $list = $('#resut_list');
+    var $list = $('#resut_list');
+
+    var baseUrlImages = "/Praxisseminar/WebProject/content/images/thumbnails/";
 
     var minDateString = "";
     var maxDateString = "";
     var journeyString = "";
     var resultTypeString = "";
-    if (urlVars.minDate) {
-        minDateString = ", min. Datum: " + utils.formatDate(urlVars.minDate);
-    }
-    if (urlVars.maxDate) {
-        maxDateString = ", max. Datum: " + utils.formatDate(urlVars.maxDate);
-    }
-    if (urlVars.journey) {
-        journeyString = ", in Reise: " + urlVars.journey;
-    }
-    if (urlVars.resultType) {
-        resultTypeString = ", Suche nach: " + utils.translate(urlVars.resultType);
-    }
 
-    dbConnector.getSearchResult(urlVars.search, urlVars.minDate, urlVars.maxDate, urlVars.journey, urlVars.resultType, setupResultList);
+    var SearchMain = {
+        initSearchMain: function() {
+            String.prototype.trunc =
+                function(n) {
+                    return this.substr(0, n - 1) + (this.length > n ? '&hellip;' : '');
+                };
 
-    function setupResultList(data) {
-        $("#search_in_progress").hide();
+            //Setup Searchheader to show what the User searched for
+            if (urlVars.minDate) {
+                minDateString = ", min. Datum: " + utils.formatDate(urlVars.minDate);
+            }
+            if (urlVars.maxDate) {
+                maxDateString = ", max. Datum: " + utils.formatDate(urlVars.maxDate);
+            }
+            if (urlVars.journey) {
+                journeyString = ", in Reise: " + urlVars.journey;
+            }
+            if (urlVars.resultType) {
+                resultTypeString = ", Suche nach: " + utils.translate(urlVars.resultType);
+            }
 
-        for (var i = 0; i < data.length; i++) {
-            addListElement(data[i]);
+            //Connect To Database and get result list
+            dbConnector.getSearchResult(urlVars.search, urlVars.minDate, urlVars.maxDate, urlVars.journey, urlVars.resultType, this.setupResultList);
+        },
+        setupResultList: function(data) {
+            $("#search_in_progress").hide();
+
+            for (var i = 0; i < data.length; i++) {
+                SearchMain.addListElement(data[i]);
+            }
+            $('#title').html(data.length + ' Ergebnisse für: "' + urlVars.search.replace('<', '&lt') + '"' + journeyString + minDateString + maxDateString + resultTypeString);
+        },
+        addListElement: function(object) {
+            var resultUrlAddition = "";
+            var idUrlAddition = "";
+            var iconURL = "";
+            var availableData = "";
+
+            var resultType = "";
+
+            //Check for Element Type
+            if (object.table_name == "songs") {
+                resultType = "result_type_audio";
+                resultUrlAddition = urlResult_constant + "songs";
+                iconURL = "img/audio-icon-black.png";
+            }
+            if (object.table_name == "images") {
+                resultType = "result_type_image"
+                resultUrlAddition = urlResult_constant + "images";
+                iconURL = baseUrlImages + object.url;
+            }
+            if (object.table_name == "videos") {
+                resultType = "result_type_video"
+                resultUrlAddition = urlResult_constant + "videos";
+                iconURL = "img/video-icon-black.png";
+            }
+            idUrlAddition = urlId_constant + object.id;
+            if (object.table_name == "stations") {
+                resultType = "result_type_station"
+                idUrlAddition = "";
+                iconURL = "img/mapmarker2.png";
+                availableData = this.getAvailableData(object);
+            }
+
+            $listItem = $('<li class="result_element"> <div><img class="result_icon" src="' + iconURL + '"></img></div> <div class="result_name">' + object.name.trunc(30) + ' || ' + object.journey + ' || Datum: ' + utils.formatDate(object.date) + availableData + '</div> </li');
+
+            $listItem.click(function() {
+                window.open("station_details.php?station_id=" + object.station_id + resultUrlAddition + idUrlAddition, "_self");
+            });
+            $list.append($listItem);
+        },
+        getAvailableData: function(station) {
+            var availableData = "<span class='available_data'> || ";
+
+            //Setup indicator which media types are present for that specific station
+            if (station.has_images == 1) {
+                availableData += "<img class='image_anchor no_botton_margin' src='img/img-icon-black.png'>";
+            }
+            if (station.has_songs == 1) {
+                availableData += "<img class='audio_anchor no_botton_margin' src='img/audio-icon-black.png'>";
+            }
+            if (station.has_videos == 1) {
+                availableData += "<img class='video_anchor no_botton_margin' src='img/video-icon-black.png'>";
+            }
+            availableData += "</span>";
+
+            return availableData;
         }
-        $('#title').html(data.length + ' Ergebnisse für: "' + urlVars.search.replace('<', '&lt') + '"' + journeyString + minDateString + maxDateString + resultTypeString);
-    }
+    };
+    SearchMain.initSearchMain();
+    return SearchMain;
 
-    function addListElement(object) {
-        var resultUrlAddition = "";
-        var idUrlAddition = "";
-        var iconURL = "";
-        var availableData = "";
-
-        var resultType = "";
-        if (object.table_name == "songs") {
-            resultType = "result_type_audio";
-            resultUrlAddition = urlResult_constant + "songs";
-            iconURL = "img/audio-icon-black.png";
-        }
-        if (object.table_name == "images") {
-            resultType = "result_type_image"
-            resultUrlAddition = urlResult_constant + "images";
-            iconURL = "/Praxisseminar/WebProject/content/images/thumbnails/" + object.url;
-        }
-        if (object.table_name == "videos") {
-            resultType = "result_type_video"
-            resultUrlAddition = urlResult_constant + "videos";
-            iconURL = "img/video-icon-black.png";
-        }
-        idUrlAddition = urlId_constant + object.id;
-        if (object.table_name == "stations") {
-            console.log(object);
-            resultType = "result_type_station"
-            idUrlAddition = "";
-            iconURL = "img/mapmarker2.png";
-            availableData = getAvailableData(object);
-        }
-
-        $listItem = $('<li class="result_element"> <div><img class="result_icon" src="' + iconURL + '"></img></div> <div class="result_name">' + object.name.trunc(30) + ' || ' + object.journey + ' || Datum: ' + utils.formatDate(object.date) + availableData + '</div> </li');
-
-        $listItem.click(function() {
-            window.open("station_details.php?station_id=" + object.station_id + resultUrlAddition + idUrlAddition, "_self");
-        });
-        $list.append($listItem);
-    }
-
-    function getAvailableData(station) {
-        var availableData = "<span class='available_data'> || ";
-
-        if (station.has_images == 1) {
-            availableData += "<img class='image_anchor no_botton_margin' src='img/img-icon-black.png'>";
-        }
-        if (station.has_songs == 1) {
-            availableData += "<img class='audio_anchor no_botton_margin' src='img/audio-icon-black.png'>";
-        }
-        if (station.has_videos == 1) {
-            availableData += "<img class='video_anchor no_botton_margin' src='img/video-icon-black.png'>";
-        }
-        availableData += "</span>";
-
-        return availableData;
-    }
 
 });
